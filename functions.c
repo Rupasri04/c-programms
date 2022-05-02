1cd01a6efd6cb61bac7dc132a972d3927623581e
Rupasri04 <104452579+Rupasri04@users.noreply.github.com>
2022-05-02 19:44:37 +0530

    Add files via upload
    the above program is addition using functions

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void add(int a,int b);
+int main()
+{
+	int a,b;
+	printf("enter the values of a and b:");
+	scanf("%d %d",&a,&b);
+	add( a,b);
+    return 0;	
+}
+void add(int a,int b)
+{
+	int c;
+	c=a+b;
+	printf("addition:%d",c);
+}
